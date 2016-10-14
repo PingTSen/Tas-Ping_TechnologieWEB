c55becadeaf0077449e8034256e8baec5dadc333
PingTSen <16333@ecam.be>
2016-10-14 12:07:09 +0200

    Add files via upload

---
+++ res/main.php
@@ -0,0 +1,32 @@
+<?php
+    
+    $html = '<!DOCTYPE html>
+             <meta charset="UTF-8"/>
+                <html>
+                    <head>
+                        <title> RESERVATION </title>
+                        <h1> RESERVATION </h1>
+                        <style>'.file_get_contents(__DIR__.'/styles.css').'</style>
+                    </head>
+                    <body>
+                    <p>Le prix de la place est de 10 euros jusqu\'à 12 ans et ensuite de 15 euros.</p>
+                    <p> Le prix de l\'assurance annulation est de 20 euros quel que soit le nombre de voyageurs.</p><br>
+                    <div id="container"> 
+                    <form method="post" action="traitement.php">
+                        <label for="destination">Destiation        :</label> <input type="text" name="destination" / ><br>
+                        <label for="number_Places">Nombre de places :</label> <input type="text" name="number_of_places" / ><br>
+                        <label for="insurance">Assurance annulation:</label> <input type="checkbox" name="insurance" / ><br>
+                    </div><br>
+                    <input type="submit" value="Etape suivante" / >
+                    <input type="submit" value="Annuler la reservation" / >
+                    </form>
+                    ';
+    $html.= '</body> </html>';
+    
+    echo $html;
+    
+
+    
+    
+
+?>
