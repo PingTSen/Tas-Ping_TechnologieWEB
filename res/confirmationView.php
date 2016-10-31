34fb2a20f1b957cfe086df2c1c9cb2a5ecaba7fd
PingTSen <16333@ecam.be>
2016-10-31 22:41:28 +0100

    View
    Nouvelle vue pour la dernière étape (4)

---
+++ res/confirmationView.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+               
+
+        <meta charset="UTF-8"/>
+		<head>
+            <title>  CONFIRMATION DES RESERVATIONS   </title>
+            <h1>     CONFIRMATION DES RESERVATIONS   </h1>
+			<link rel="stylesheet" href='styles.css' />
+        </head>
+        <body>
+            <p> Votre demande à bien été enregistrée. <br></p>
+            <p> Merci de bien vouloir verser la somme de sur le comptes 000-000000-00 <br></p>
+            <form method="post" action="controls.php">
+                <a href="#" class="button">
+                    <label>
+						<span class="confirme">
+								Retour à la page d'accueil
+								<input type="submit" name = 'confirme'  style="display:none"  />
+						</span>
+                    </label>
+				</a>
+            </form>>
+        </body>
+</html>
