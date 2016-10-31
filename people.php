3dd10bb9e083af7f798af3543d891809e3fc5c68
PingTSen <16333@ecam.be>
2016-10-31 19:44:58 +0100

    Update v3-04
    -conservation donnees

---
+++ people.php
@@ -0,0 +1,45 @@
+<?php
+
+ /* Class for a people
+ This class has all the info about one personne */
+
+   class people{
+	   
+	   private $age;
+	   private $name;
+	   
+	   
+	   public function __construct($age,$name)
+	   {
+		   
+		   $this->age = $age;
+		   $this->name=$name;
+		   
+		   
+	   }
+	   
+	   public function getName()
+	   {
+		   return $this->name;
+	   }
+	   
+	   public function getAge()
+	   {
+		   return $this->age;
+	   }
+	   
+	   public function setName($name)
+	   {
+		   $this->name=$name;
+	   }
+	   
+	   public function setAge($age)
+	   {
+		    $this->age=$age;
+	   }
+   }
+
+
+
+
+?>
