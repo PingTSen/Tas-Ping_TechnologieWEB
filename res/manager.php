5934ee930afac8f11473ba1724a87296c47f7f00
PingTSen <16333@ecam.be>
2016-12-16 12:10:33 +0100

    manager

--- res/manager.php
+++ res/manager.php
@@ -6,6 +6,7 @@
                 <h1>     LISTE DES RESERVATIONS   </h1>
         </head>
         <body>
+		<form method="post" action="index.php">
             <a href="#" class="button">
                 <label>
                     <span class="ajoutReserv" > <!-- a rajouter dans CSS-->
@@ -14,6 +15,7 @@
                     </span>
                 </label>
             </a>
+		</form>
             <br>
             <!-- Création du tableau-->
             <table>
@@ -46,8 +48,30 @@
                             echo "<td>".$line['Destination']."</td>";
                             echo "<td>".$line['Assurance']."</td>";
                             echo "<td>".$line['Total']."</td>";
-                            echo "<td>".$line['Nom - Age']."</td>";
+                            echo "<td>".$line['NA']."</td>";
+							echo "<td><form method=/'post/' action='index.php'>
+								 <a href=/'#/' class=/'button/'>
+								<label>
+								<span class=/'Edition/' > <!-- a rajouter dans CSS-->
+								Editer
+								<input type='submit' name = 'edition'  style='display:none 'value='chiiffre' />
+								<input type='hidden' name='page' value='controls' />
+								</span>
+								</label>
+								</a>
+								</form></td>";
+							echo "<td><form method=/'post/' action=/'index.php/'>
+							 <a href=/'#/' class=/'button/'>
+							<label>
+							<span class=/'Delete/' > <!-- a rajouter dans CSS-->
+							Supprimer
+							<input type='submit' name = 'delete'  style='display:none' />
+							</span>
+							</label>
+							</a>
+							</form></td>";
                             echo "</tr>\n";
+							
                         }
                     }
                         
